3b312423383a60024f2551e7ff3e7e6753acfe33
Cyber-Alexei <150178624+Cyber-Alexei@users.noreply.github.com>
2025-02-04 23:17:26 -0600

    banner in the landing was created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Section1 from "@/pages/home/section1";
+
 export default function Home() {
-  return <div>Hello worldfff!</div>;
+  return (
+    <div className="w-full h-auto min-h-[100vh]">
+      <Section1 />
+    </div>
+  );
 }
